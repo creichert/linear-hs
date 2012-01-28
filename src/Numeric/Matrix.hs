4d136cf9c02b261e16efe8eca4628159c4333d5c
Christopher Reichert <creichert07@gmail.com>
2012-01-28 01:20:36 -0600

    Implement Matrix as instance of Num.
    Create a mkMatrix function to generate matrices.

--- src/Numeric/Matrix.hs
+++ src/Numeric/Matrix.hs
@@ -7,7 +7,7 @@
 ---- Maintainer : Christopher Reichert <creichert07@gmail.com>
 ---- Stability : experimental
 ----
----- Vector
+---- Matrix
 ----
 ---- It can be assumed that these operations are undefined for Vectors
 ---- of unequal length.
@@ -15,7 +15,27 @@
 
 module Numeric.Matrix(
         Matrix(..)
+      , mkMatrix
       ) where
 
 data Matrix a = Matrix Int Int [[a]]
+    deriving (Show)
+
+instance Eq a => Eq (Matrix a) where
+    (==) (Matrix r1 c1 xs) (Matrix r2 c2 ys) = r1 == r2 && c1 == c2 && xs == ys
+
+instance (Num a) => Num (Matrix a) where
+    (+)         = error "Undefined operation for matrices"
+    (-)         = error "Undefined operation for matrices."
+    (*)         = error "Undefined operation for matrices."
+    abs         = error "Undefined operation for matrices."
+    signum      = error "Undefined operation for matrices."
+    fromInteger = error "Undefined operation for matrices."
+
+-- | Create a new matrix from a list of lists of Int.
+mkMatrix :: (Num a) => [[a]] -> Matrix a
+mkMatrix [] = Matrix 0    0    [[0]]
+mkMatrix xs = Matrix rows cols xs
+    where rows = length xs
+          cols = length $ head xs
 
--- test/Main.hs
+++ test/Main.hs
@@ -14,9 +14,15 @@ import Test.HUnit
 import Test.QuickCheck
 
 import Test.Vector
+import Test.Matrix
 
 main = do putStrLn "Running vector quick tests."
           mapM_ (\(s,a) -> printf "%-25s: " s >> a) qtests_Vector
           putStrLn "Running vector unit tests."
           runTestTT utests_Vector
+
+          putStrLn "Running matrix quick tests."
+          mapM_ (\(s,a) -> printf "%-25s: " s >> a) qtests_Matrix
+          putStrLn "Running vector unit tests."
+          runTestTT utests_Matrix
 
